1257ee70c0df30f0624cdc337e96489724d78e57
Alejandro Villegas <alex.ansi.c@gmail.com>
2023-11-29 14:00:27 +0100

    Added table sorting to Clusters and ClusterDetails
    Signed-off-by: Alejandro Villegas <alex.ansi.c@gmail.com>

--- src/app/ClusterDetails/ClusterDetails.tsx
+++ src/app/ClusterDetails/ClusterDetails.tsx
@@ -21,7 +21,7 @@ import {
   LabelGroup,
 } from "@patternfly/react-core";
 import InfoCircleIcon from "@patternfly/react-icons/dist/js/icons/info-circle-icon";
-import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
+import { Table, Tbody, Td, Th, Thead, Tr, ThProps } from "@patternfly/react-table";
 import { getCluster, getClusterInstances, getClusterTags } from "../services/api";
 import { ClusterData, Instance, Tag, TagData } from "@app/types/types";
 import { Link, useLocation  } from "react-router-dom";
@@ -77,6 +77,54 @@ const AggregateInstancesPerCluster: React.FunctionComponent = () => {
 
   console.log("Rendered with data:", data);
 
+  //### Sorting ###
+  // Index of the currently active column
+  const [activeSortIndex, setActiveSortIndex] = React.useState<number | undefined>(0);
+  // sort direction of the currently active column
+  const [activeSortDirection, setActiveSortDirection] = React.useState<'asc' | 'desc' | undefined>('asc');
+  // sort dropdown expansion
+  const getSortableRowValues = (instance: Instance): (string | number | null)[] => {
+    const { id, name, availabilityZone, instanceType, state, clusterID, provider } = instance;
+    return [id, name, availabilityZone, instanceType, state, clusterID, provider];
+  };
+
+  // Sorting
+  let sortedData = data;
+  if (typeof activeSortIndex === 'number' && activeSortIndex !== null) {
+    sortedData = data.sort((a, b) => {
+      const aValue = getSortableRowValues(a)[activeSortIndex];
+      const bValue = getSortableRowValues(b)[activeSortIndex];
+      if (typeof aValue === 'number') {
+        // Numeric sort
+        if (activeSortDirection === 'asc') {
+          return (aValue as number) - (bValue as number);
+        }
+        return (bValue as number) - (aValue as number);
+      } else {
+        // String sort
+        if (activeSortDirection === 'asc') {
+          return (aValue as string).localeCompare(bValue as string);
+        }
+        return (bValue as string).localeCompare(aValue as string);
+      }
+    });
+  }
+
+  // set table column properties
+  const getSortParams = (columnIndex: number): ThProps['sort'] => ({
+    sortBy: {
+      index: activeSortIndex,
+      direction: activeSortDirection,
+      defaultDirection: 'asc' // starting sort direction when first sorting a column. Defaults to 'asc'
+    },
+    onSort: (_event, index, direction) => {
+      setActiveSortIndex(index);
+      setActiveSortDirection(direction);
+    },
+    columnIndex
+  });
+  //### --- ###
+
   return (
     <React.Fragment>
       {loading ? (
@@ -94,15 +142,15 @@ const AggregateInstancesPerCluster: React.FunctionComponent = () => {
         <Table aria-label="Simple table">
           <Thead>
             <Tr>
-              <Th>ID</Th>
-              <Th>Name</Th>
-              <Th>Type</Th>
-              <Th>AvailabilityZone</Th>
-              <Th>State</Th>
+              <Th sort={getSortParams(0)}>ID</Th>
+              <Th sort={getSortParams(1)}>Name</Th>
+              <Th sort={getSortParams(3)}>Type</Th>
+              <Th sort={getSortParams(2)}>AvailabilityZone</Th>
+              <Th sort={getSortParams(4)}>State</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {data.map((instance) => (
+            {sortedData.map((instance) => (
               <Tr key={instance.id}>
                 <Td dataLabel={instance.id}>
                   <Link
